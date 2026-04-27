db159b1444d0e9e39e96ad3f2543393f321e5bd9
Gamal Osama <gamal5osama66@gmail.com>
2026-04-27 23:44:43 +0300

    adding the enums PesponseSignal to use them in the data route

---
+++ source/models/enums/ResponseEnums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class ResponseSignal(Enum):
+
+    # the following signals are enums valus you should use .value when calling to get the string value of the signal
+    FILE_VALIDATION_SUCCESS = "file_validation_success"
+
+    FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
+    FILE_SIZE_EXCEEDS_LIMIT = "file_size_exceeds_limit"
+    
+    FILE_UPLOADED_SUCCESSFULLY = "file_uploaded_successfully"
+    FILE_UPLODED_FAILED = "file_uploaded_failed"
