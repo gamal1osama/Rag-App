c0550fa67cfac308f13dba9a603297ca8b61179f
Gamal Osama <gamal5osama66@gmail.com>
2026-04-29 03:23:36 +0300

    update the name of the field to be more descriptive

--- source/routes/schemas/data.py
+++ source/routes/schemas/data.py
@@ -6,7 +6,7 @@ class ProcessRequest(BaseModel):
     file_id: str
     
     chunk_size: Optional[int] = 100
-    overlap_size: Optional[int] = 20
+    chunk_overlap: Optional[int] = 20
 
     do_reset: Optional[int] = 0
     
