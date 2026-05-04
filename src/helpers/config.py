0210b19a43adea73e888c0ffbb5c3a38e3463641
Gamal Osama <gamal5osama66@gmail.com>
2026-05-04 03:56:55 +0300

    update the config to match the .env

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -6,14 +6,28 @@ class Settings(BaseSettings):
     APP_NAME: str 
     APP_VERSION: str
 
-    OPENAI_API_KEY: str
-
     FILE_ALLOWED_TYPES: list[str]
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
 
     MONGODB_URL: str
     MONGODB_DATABASE: str
+
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str=None
+    OPENAI_BASE_URL: str=None
+
+    COHERE_API_KEY: str=None
+
+    GENERATION_MODEL_ID: str=None
+    EMBEDDING_MODEL_ID: str=None
+    EMBEDDING_MODEL_SIZE: int=None
+
+    DEFAULT_INPUT_MAX_CHARACTERS: int=None
+    DEFAULT_OUTPUT_MAX_TOKENS: int=None
+    DEFAULT_TEMPERATURE: float=None
 
     class Config:
         env_file = ".env"
